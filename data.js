4d7895c22eeabe0c3b66de5d3b6fd13eedaa698d
mariodiez12 <mario.diez.dev@gmail.com>
2017-04-01 00:12:51 +0200

    updated data.js

--- data.js
+++ data.js
@@ -22,5 +22,21 @@ export default recipes = [
         title: 'Coconut macaroons is truly an easy recipe',
         user: 'https://0.gravatar.com/avatar/9b1a243cd301d1ba99865937f4e7cc94?s=130&d=mm&r=g',
         by: 'Kimberly',
+    },
+     {
+        id: 4,
+        image: 'https://www.fivelittlechefs.com/wp-content/uploads/2016/02/valentine-coconut-macaroons.jpg',
+        food: 'Coconut Macaroons',
+        title: 'Coconut macaroons is truly an easy recipe',
+        user: 'https://0.gravatar.com/avatar/9b1a243cd301d1ba99865937f4e7cc94?s=130&d=mm&r=g',
+        by: 'Kimberly',
+    },
+     {
+        id: 5,
+        image: 'https://www.fivelittlechefs.com/wp-content/uploads/2016/02/valentine-coconut-macaroons.jpg',
+        food: 'Coconut Macaroons',
+        title: 'Coconut macaroons is truly an easy recipe',
+        user: 'https://0.gravatar.com/avatar/9b1a243cd301d1ba99865937f4e7cc94?s=130&d=mm&r=g',
+        by: 'Kimberly',
     }
 ]
